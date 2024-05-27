65ff341116b7c74ccc0fd15be32e57b29fc5bcad
jingmin1119 <149864787+jingmin1119@users.noreply.github.com>
2024-05-27 14:27:54 +0800

    Add files via upload

---
+++ src/model/Model_playerRanking.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class Model_playerRanking {
+    int playerID,rank,games;
+    String name,position;
+    double compositeScore,steals,blocks,rebounds,assits;
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Model_playerRanking(int playerID, int rank, int games, String name, String position, double compositeScore, double steals, double blocks, double rebounds, double assits) {
+        this.playerID = playerID;
+        this.rank = rank;
+        this.games = games;
+        this.name = name;
+        this.position = position;
+        this.compositeScore = compositeScore;
+        this.steals = steals;
+        this.blocks = blocks;
+        this.rebounds = rebounds;
+        this.assits = assits;
+    }
+
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCompositeScore() {
+        return compositeScore;
+    }
+
+    public void setPlayerID(int playerID) {
+        this.playerID = playerID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCompositeScore(double compositeScore) {
+        this.compositeScore = compositeScore;
+    }
+
+    public int getGames() {
+        return games;
+    }
+
+    public void setGames(int games) {
+        this.games = games;
+    }
+
+    public double getSteals() {
+        return steals;
+    }
+
+    public void setSteals(double steals) {
+        this.steals = steals;
+    }
+
+    public double getBlocks() {
+        return blocks;
+    }
+
+    public void setBlocks(double blocks) {
+        this.blocks = blocks;
+    }
+
+    public double getRebounds() {
+        return rebounds;
+    }
+
+    public void setRebounds(double rebounds) {
+        this.rebounds = rebounds;
+    }
+
+    public double getAssits() {
+        return assits;
+    }
+
+    public void setAssits(double assits) {
+        this.assits = assits;
+    }
+    
+}  
+
