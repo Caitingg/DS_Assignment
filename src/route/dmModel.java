bc1785af9d217ac8ac889a6931694ee421fa6db7
Caitingg <144991651+Caitingg@users.noreply.github.com>
2024-05-29 15:48:00 +0800

    Update dmModel.java

--- src/route/dmModel.java
+++ src/route/dmModel.java
@@ -4,62 +4,65 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
+public class dmModel extends Application {
 
-public class dmModel extends Application
-{
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
 
+        ArrayList<String> cities = new ArrayList<>(
+                Arrays.asList("Los Angeles", "Golden State", "Oklahoma City", "Phoenix", "Denver",
+                        "Houston", "San Antonio", "Boston", "Orlando", "Miami", "Seattle", "San Francisco")
+        );
 
+        City[] vertices = {
+                new City("Los Angeles", 100, 300),
+                new City("Golden State", 150, 100),
+                new City("Oklahoma City", 400, 350),
+                new City("Phoenix", 250, 300),
+                new City("Denver", 450, 200),
+                new City("Houston", 600, 400),
+                new City("San Antonio", 350, 500),
+                new City("Boston", 850, 100),
+                new City("Orlando", 700, 500),
+                new City("Miami", 850, 600),
+                new City("Seattle", 600, 150),
+                new City("San Francisco", 400, 100)
+        };
 
-        ArrayList<String> cities = new ArrayList<>
-                (
-                        Arrays.asList("Seattle", "San Francisco", "Los Angeles", "Denver", "Kansas City",
-                                "Chicago", "Boston", "New York", "Atlanta", "Miami", "Dallas", "Houston")
-                );
-        City[] vertices =
-                {
-                        new City("Seattle", 75, 50),
-                        new City("San Francisco", 50, 210),
-                        new City("Los Angeles", 75, 275), new City("Denver", 275, 175),
-                        new City("Kansas City", 400, 245),
-                        new City("Chicago", 450, 100), new City("Boston", 700, 80),
-                        new City("New York", 675, 120), new City("Atlanta", 575, 295),
-                        new City("Miami", 600, 400), new City("Dallas", 408, 325),
-                        new City("Houston", 450, 360)
-                };
-
-        int[][] edges =
-                {
-                        {0, 1, 807}, {0, 3, 1331}, {0, 5, 2097},
-                        {1, 0, 807}, {1, 2, 381}, {1, 3, 1267},
-                        {2, 1, 381}, {2, 3, 1015}, {2, 4, 1663}, {2, 10, 1435},
-                        {3, 0, 1331}, {3, 1, 1267}, {3, 2, 1015}, {3, 4, 599},
-                        {3, 5, 1003},
-                        {4, 2, 1663}, {4, 3, 599}, {4, 5, 533}, {4, 7, 1260},
-                        {4, 8, 864}, {4, 10, 496},
-                        {5, 0, 2097}, {5, 3, 1003}, {5, 4, 533},
-                        {5, 6, 983}, {5, 7, 787},
-                        {6, 5, 983}, {6, 7, 214},
-                        {7, 4, 1260}, {7, 5, 787}, {7, 6, 214}, {7, 8, 888},
-                        {8, 4, 864}, {8, 7, 888}, {8, 9, 661},
-                        {8, 10, 781}, {8, 11, 810},
-                        {9, 8, 661}, {9, 11, 1187},
-                        {10, 2, 1435}, {10, 4, 496}, {10, 8, 781}, {10, 11, 239},
-                        {11, 8, 810}, {11, 9, 1187}, {11, 10, 239}
-                };
-
-
+        int[][] edges = {
+                // Los Angeles
+                {0, 1, 554}, {0, 2, 1901}, {0, 5, 577}, {0, 10, 1137}, {0, 11, 382},
+                // Golden State
+                {1, 4, 1507}, {1, 2, 2214}, {1, 10, 807}, {1, 11, 348},
+                // Oklahoma City
+                {2, 4, 942}, {2, 6, 678}, {2, 5, 778},
+                // Phoenix
+                {3, 6, 500}, {3, 0, 357},
+                // Denver
+                {4, 7, 2845}, {4, 10, 1326},
+                // Houston
+                {5, 7, 2584}, {5, 8, 458}, {5, 6, 983},
+                // San Antonio
+                {6, 8, 1137}, {6, 9, 1370},
+                // Boston
+                {7, 9, 3045}, {7, 8, 1254},
+                // Orlando
+                {8, 9, 268}, {8, 11, 2436},
+                // Miami
+                {9, 10, 2731},
+                // Seattle
+                {10, 11, 807}, {10, 4, 1326}, {10, 0, 1137},
+                // San Francisco
+                {11, 0, 382}, {11, 1, 348}, {11, 8, 2436}
+        };
 
         WeightedGraph<City> graph = new WeightedGraph<>(vertices, edges);
 
@@ -76,88 +79,89 @@ public class dmModel extends Application
         settings.setAlignment(Pos.CENTER);
 
         settings.getChildren().add(new Label("Starting City: "));
-        TextField startingCity = new TextField();
-        startingCity.setPrefColumnCount(12);
-        settings.getChildren().add(startingCity);
+        ComboBox<String> startCityComboBox = new ComboBox<>();
+        startCityComboBox.getItems().addAll(cities);
+        startCityComboBox.setPrefWidth(150);
+        settings.getChildren().add(startCityComboBox);
 
         settings.getChildren().add(new Label("Ending City: "));
-        TextField endingCity = new TextField();
-        endingCity.setPrefColumnCount(12);
-        settings.getChildren().add(endingCity);
+        ComboBox<String> endCityComboBox = new ComboBox<>();
+        endCityComboBox.getItems().addAll(cities);
+        endCityComboBox.setPrefWidth(150);
+        settings.getChildren().add(endCityComboBox);
 
         Button displayShortestPath = new Button("Display Shortest Path");
         settings.getChildren().add(displayShortestPath);
         pane.getChildren().add(settings);
 
-        displayShortestPath.setOnAction(e ->
-        {
+        displayShortestPath.setOnAction(e -> {
             status.setText("");
-            int index1 = cities.indexOf(startingCity.getText().trim());
-            int index2 = cities.indexOf(endingCity.getText().trim());
+            String startCity = startCityComboBox.getValue();
+            String endCity = endCityComboBox.getValue();
 
-            if (index1 == -1)
-            {
-                status.setText("City " + startingCity.getText() + " is not exist");
+            if (startCity == null || endCity == null) {
+                status.setText("Please select both starting and ending cities.");
                 return;
             }
 
-            if (index2 == -1)
-            {
-                status.setText("City " + endingCity.getText() + " is not exist");
+            int index1 = cities.indexOf(startCity);
+            int index2 = cities.indexOf(endCity);
+
+            if (index1 == -1) {
+                status.setText("City " + startCity + " does not exist.");
                 return;
             }
 
-            try
-            {
-                graphView.displayShortestPath(index1,  index2);
+            if (index2 == -1) {
+                status.setText("City " + endCity + " does not exist.");
+                return;
             }
-            catch (Exception exception)
-            {
+
+            // For demonstration, printing the selected cities
+            System.out.println("Start City: " + startCity);
+            System.out.println("End City: " + endCity);
+
+            // Placeholder for calling the method to display the shortest path
+            try {
+                graphView.displayShortestPath(index1, index2);
+            } catch (Exception exception) {
                 exception.printStackTrace();
             }
         });
 
-
-
         Scene scene = new Scene(pane, 750, 500);
-        primaryStage.setTitle("City Digital Model");
+        primaryStage.setTitle("Shortest Path Visualization");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-
-    static class City implements Displayable
-    {
+    static class City implements Displayable {
         private double x, y;
         private String name;
 
-        City(String name, double x, double y)
-        {
+        City(String name, double x, double y) {
             this.name = name;
             this.x = x;
             this.y = y;
         }
 
         @Override
-        public double getX()
-        {
+        public double getX() {
             return x;
         }
 
         @Override
-        public double getY()
-        {
+        public double getY() {
             return y;
         }
 
-
         @Override
-        public String getName()
-        {
+        public String getName() {
             return name;
         }
     }
 }
+
 
 
 
